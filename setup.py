041d9fcb47005112ea0e0e9006fd4e041f7979b1
Alexander Bogushov <abogushov@gmail.com>
2019-02-08 15:33:56 +0500

    👷 Still not work. Remove testing over python 3.8.

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     test_suite='tests',
     tests_require=test_requirements,
